d28d5c3a75df7f89369e18be3b5f4bd4f8206a09
dimasprastyoaditama <prastyod647@gmail.com>
2025-05-27 19:07:41 +0700

    Membuat class ibu

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -1,31 +1,20 @@
 #ifndef IBU_H
 #define IBU_H
 
-class ibu {
+class ibu{
     public:
-       string nama;
-       vector<anak*> daftar_anak;
+        string nama;
+        vector<anak*> daftar_anak;
 
-       ibu(string pNama): nama(pNama){
-           cout << "Ibu \"" << nama << "\" ada\n";
-       }
-
-       ~ibu() {
-            cout << "Ibu" \"" << nama << "\" tidak ada\n";
-       }
-
-       void tambahAnak(anak*);
-       void cetakAnak();
-    };
-    void ibu::cetakAnak() {
-        cout << "Daftar Anak dari Ibu \"" <<
-        this->nama << "\":\n";
-        for (int i = 0; i < daftar_anak.size(); i++) {
-            cout << daftar_anak[i]->nama << endl;
+        ibu (string pNama): nama(pNama){
+            cout << "ibu\"" << nama << "\" ada\n";
         }
-        cout << endl;
-    }
 
-    endif
+        ~ibu(){
+            cout << "ibu\"" << nama << "\" tidak ada\n";
+        }
 
+        void tambahAnak(anak*);
+        void cetakAnak();
+};
 
--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -14,4 +14,53 @@ class pasien{
             cout << "Pasien \"" << nama
             << "\" tidak ada\n";
         }
+
+        void tambahDokter(dokter*);
+        void cetakDokter();
 };
+
+class dokter {
+    public:
+        string nama;
+        vector<pasien*> daftar_pasien;
+
+        dokter(string pNama): nama(pNama) {
+            cout << "Dokter \"" << nama << "\" ada\n";
+        }
+        ~dokter() {
+            cout << "Dokter \"" << nama 
+            << "\" tidak ada\n"; 
+        }
+
+        void tambahPasien(pasien*)
+        void cetakPsien();
+};
+
+    void paien::tambahDokter(dokter* pDokter) {
+        daftar_dokter.push_back(pDokter);
+    }
+
+    void pasien::cetakDokter() {
+        cout << "Daftar dokter yang menangani pasien \""
+        << this->nama << "\":\n";
+        //auto digunakan dalam perulangan
+        for (auto& a : daftar_dokter) {
+            cout << a->nama << "\n";
+        }
+        cout << endl;
+    }
+
+    int main() {
+        dokter* varDokter1 = new dokter("dr.Budi");
+        dokter* varDokter2 = new dokter("dr.Tono");
+        pasien* varPasien1 = new pasien("Andi");
+        pasien* varPasien 
+    }
+
+
+
+
+    
+
+
+
