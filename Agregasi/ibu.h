3fe0319b004771fa287e5d4c3c079d2ccb909bc8
dimasprastyoaditama <prastyod647@gmail.com>
2025-05-27 19:09:08 +0700

    Membuat void ibu

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -18,3 +18,16 @@ class ibu{
         void cetakAnak();
 };
 
+        void ibu ::tambahAnak(anak* pAnak){
+        daftar_anak.push_back(pAnak);
+}
+
+        void ibu ::cetakAnak(){
+        cout << "Daftar Anak dari IBU \"" << this->nama<< "\":\n";
+        for (int i=0; i < daftar_anak.size(); i++){
+        cout << daftar_anak[i]->nama<<endl;
+    }
+        cout << endl;
+}
+#endif
+
